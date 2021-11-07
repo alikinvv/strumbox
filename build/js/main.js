fea76d022c5322454e408e3753ee946a583cad07
Valery Alikin <weblcw@gmail.com>
2021-11-07 13:35:13 +0500

    update

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,10 @@
 "use strict";
 
-$('body').on('click', '', function () {});
+$('body').on('click', '.counter__control.plus', function (e) {
+  $(e.currentTarget).parent().find('input').val(parseInt($(e.currentTarget).parent().find('input').val()) + 1);
+});
+$('body').on('click', '.counter__control.minus', function (e) {
+  if ($(e.currentTarget).parent().find('input').val() > 1) {
+    $(e.currentTarget).parent().find('input').val(parseInt($(e.currentTarget).parent().find('input').val()) - 1);
+  }
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
-$('body').on('click', '', () => {});
+$('body').on('click', '.counter__control.plus', (e) => {
+    $(e.currentTarget)
+        .parent()
+        .find('input')
+        .val(parseInt($(e.currentTarget).parent().find('input').val()) + 1);
+});
+
+$('body').on('click', '.counter__control.minus', (e) => {
+    if ($(e.currentTarget).parent().find('input').val() > 1) {
+        $(e.currentTarget)
+            .parent()
+            .find('input')
+            .val(parseInt($(e.currentTarget).parent().find('input').val()) - 1);
+    }
+});
